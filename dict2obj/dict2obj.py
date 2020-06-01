5be7c7031fe60493775629f19e0c0d3d3b521522
Restyled.io <commits@restyled.io>
2020-06-01 11:56:40 +0000

    Restyled by autopep8

--- dict2obj/dict2obj.py
+++ dict2obj/dict2obj.py
@@ -1,34 +1,54 @@
-class Container: pass
+class Container:
+    pass
+
 
 def revert(obj):
-    if isinstance(obj, Container): return revert_container(obj)
-    elif isinstance(obj, dict): return revert_dict(obj)
-    elif isinstance(obj, list): return revert_list(obj)
-    elif isinstance(obj, tuple): return revert_tuple(obj)
-    elif isinstance(obj, set): return revert_set(obj)
-    else: return obj
+    if isinstance(obj, Container):
+        return revert_container(obj)
+    elif isinstance(obj, dict):
+        return revert_dict(obj)
+    elif isinstance(obj, list):
+        return revert_list(obj)
+    elif isinstance(obj, tuple):
+        return revert_tuple(obj)
+    elif isinstance(obj, set):
+        return revert_set(obj)
+    else:
+        return obj
+
 
 def revert_container(obj_container: Container):
     return dict([key, revert(value)] for key, value in obj_container.__dict__.items())
 
+
 def revert_dict(obj_dict: dict):
     return dict([key, revert(value)] for key, value in obj_dict.items())
+
 
 def revert_list(obj_list: list):
     return list(revert(element) for element in obj_list)
 
+
 def revert_tuple(obj_tuple: tuple):
     return tuple(revert(element) for element in obj_tuple)
+
 
 def revert_set(obj_set: set):
     return set(revert(element) for element in obj_set)
 
+
 def convert(obj):
-    if isinstance(obj, dict): return convert_dict(obj)
-    elif isinstance(obj, list): return convert_list(obj)
-    elif isinstance(obj, tuple): return convert_tuple(obj)
-    elif isinstance(obj, set): return convert_set(obj)
-    else: return obj
+    if isinstance(obj, dict):
+        return convert_dict(obj)
+    elif isinstance(obj, list):
+        return convert_list(obj)
+    elif isinstance(obj, tuple):
+        return convert_tuple(obj)
+    elif isinstance(obj, set):
+        return convert_set(obj)
+    else:
+        return obj
+
 
 def convert_dict(obj_dict: dict):
     converted = Container()
@@ -38,11 +58,14 @@ def convert_dict(obj_dict: dict):
 
     return converted
 
+
 def convert_list(obj_list: list):
     return list(convert(element) for element in obj_list)
 
+
 def convert_tuple(obj_tuple: tuple):
     return tuple(convert(element) for element in obj_tuple)
+
 
 def convert_set(obj_set: set):
     return set(convert(element) for element in obj_set)
